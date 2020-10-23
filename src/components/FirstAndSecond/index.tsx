6d6cf1a8e8d73968128c364efabeef367fc576c1
dannyelgjl <dannyelgjl@gmail.com>
2020-10-23 03:24:22 -0300

    finalizando a animação

--- src/components/FirstAndSecond/index.tsx
+++ src/components/FirstAndSecond/index.tsx
@@ -8,23 +8,23 @@ const FirstAndSecond: React.FC = () => {
 
   const frameOpacity = useTransform(scrollYProgress, [0.196, 0.198], [0, 1])
   const frameScale = useTransform(scrollYProgress, [0.558, 0.627], [0.511, 0.8])
-  
+
   return (
     <Sticky className="second">
-      <First /> 
-      <motion.div 
-      style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        height: '100vh',
-        borderRadius: '4px',
-        border: '4px solid #fff',
-        opacity: frameOpacity,
-        scale: frameScale,
-      }}
-       />
+      <First />
+      <motion.div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          height: '100vh',
+          borderRadius: '4px',
+          border: '4px solid #fff',
+          opacity: frameOpacity,
+          scale: frameScale,
+        }}
+      />
     </Sticky>
   );
 };
@@ -33,54 +33,67 @@ const FirstAndSecond: React.FC = () => {
 const First: React.FC = () => {
   const { scrollYProgress } = useViewportScroll();
 
-  const firstScale = useTransform(scrollYProgress, 
+  const firstScale = useTransform(scrollYProgress,
     [0.198, 0.264, 0.558, 0.627],
     [1, 0.511, 0.511, 1]
-    );
+  );
 
-  
-  const firstRadius = useTransform(scrollYProgress, 
+
+  const firstRadius = useTransform(scrollYProgress,
     [0.198, 0.264, 0.558, 0.627],
     [0, 4, 4, 0]
-    );  
+  );
 
-  const leftSideHeight = useTransform(scrollYProgress, 
+  const leftSideHeight = useTransform(scrollYProgress,
     [0, 0.058],
     ['20vh', '100vh']
-    )  ;
+  );
 
-  const rightSideScale = useTransform(scrollYProgress, 
+  const rightSideScale = useTransform(scrollYProgress,
     [0.047, 0.093],
     [0, 0.511]
-    );  
+  );
 
-    const rightSideY = useTransform(scrollYProgress, 
-      [0.047, 0.093],
-      ['58vh', '0vh']
-      );  
+  const rightSideY = useTransform(scrollYProgress,
+    [0.047, 0.093],
+    ['58vh', '0vh']
+  );
+
+  const offsetY = useTransform(
+    scrollYProgress,
+    [0.328, 0.397, 0.461, 0.53],
+    ["0%", "-100%", "-100%", "-200%"]
+  )
 
   return (
-    
-    <Sticky className="first" style={{ scale:firstScale, borderRadius: firstRadius }}> 
-      <div className="a">
-        <motion.div 
-          className="left-side"  
-          style={{ 
-            height: leftSideHeight 
-            }} 
+
+    <Sticky
+      className="first"
+      style={{ scale: firstScale, borderRadius: firstRadius }}
+    >
+      <motion.div className="offset" style={{
+        y: offsetY
+      }} >
+        <div className="a">
+          <motion.div
+            className="left-side"
+            style={{
+              height: leftSideHeight
+            }}
           />
-        <div className="right-side">
-          <motion.div  
-          className="right-image"
-          style={{ 
-            y: rightSideY, 
-            scale: rightSideScale 
-            }} 
-          />
+          <div className="right-side">
+            <motion.div
+              className="right-image"
+              style={{
+                y: rightSideY,
+                scale: rightSideScale
+              }}
+            />
+          </div>
         </div>
-      </div>
-      <div className="b"></div>
-      <div className="c"></div>
+        <div className="b"></div>
+        <div className="c"></div>
+      </motion.div>
     </Sticky>
   );
 };
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,24 +4,25 @@ import { Main, Section, Sticky } from './styles';
 import FirstAndSecond from '../FirstAndSecond';
 
 const Home = () => {
+
   React.useEffect(() => {
-    window.scrollTo({ top: 0 })
+    window.scrollTo({ top: 0 });
   }, []);
 
   return (
     <>
-      <Main style={{ height:'1610vh' }} >
-        <Section  style={{ height: '72.7%' }}>
+      <Main style={{ height: '1610vh' }} >
+        <Section style={{ height: '72.7%' }}>
           <FirstAndSecond />
         </Section>
 
-        <Section style={{ height: '9.7%'}} >
+        <Section style={{ height: '9.7%' }} >
           <Sticky className="third" />
         </Section>
 
 
-        <Section style={{ height: '10.1%'}} > 
-          <Sticky  className="fourth" />
+        <Section style={{ height: '10.1%' }} >
+          <Sticky className="fourth" />
         </Section>
       </Main>
     </>
